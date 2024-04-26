fc81318a31e8902772c47892ecbc7d8eb77d0188
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-26 15:55:53 +0530

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+console.log(2>1)
+console.log(2<1);
+console.log(12>=23);
+console.log(12<=23);
+
+console.log("3"==3);
+console.log("03"<4);
+
+console.log(null > 0);  // equality check == and comparison check works differently
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(2/0);   // infinity
+
+// strict comparison
+
+console.log("2" === 2);     // it checks the datatype also
