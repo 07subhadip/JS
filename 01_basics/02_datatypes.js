6d2824334e95ef7f8685da25312897212c3aa261
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-26 12:48:12 +0530

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3+3);   // code readability should be high
+
+console.log("subhadip");
+
+let name = "subhadip"
+let age = 23
+let isValid = true
+
+console.table([name,age,isValid])
+
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
