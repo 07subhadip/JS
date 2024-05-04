672cd3e17546d0ade1cdebdff15f996eaae02487
07subhadip <subhadiphensh31102000@gmail.com>
2024-05-04 17:40:04 +0530

    switch case control flow

---
+++ 04_control_flow/02_Switch_case.js
@@ -0,0 +1,16 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`february`);
+        break;
+    case 3:
+        console.log(`march`);
+        break;
+    default:
+        console.log(`default value...`);
+        break;
+}
