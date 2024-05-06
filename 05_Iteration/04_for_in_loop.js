4df8eece6cae0230ea3f9c2dd122f74b9a677bf1
07subhadip <subhadiphensh31102000@gmail.com>
2024-05-06 18:04:01 +0530

    for in loop done

---
+++ 05_Iteration/04_for_in_loop.js
@@ -0,0 +1,28 @@
+const myObject = {
+    name: "subhadip",
+    class : "MSc",
+    subject: "Compter Science"
+}
+
+for (const key in myObject) {
+    console.log(`${key} : ${myObject[key]}`);
+}
+
+const myArr = [1,2,3,4,5]
+
+for (const key in myArr) {
+    console.log(myArr[key]);
+}
+
+// const map = new Map()
+// map.set('IN','INDIA')
+// map.set('AUS','Australia')
+// map.set('NZ','New Zeland')
+
+// console.log(map);
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// this is not iteratable
