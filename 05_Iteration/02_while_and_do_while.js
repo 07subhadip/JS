20bb3fb39dd3e3d33e437509f411c1c8c0c4a4e5
07subhadip <subhadiphensh31102000@gmail.com>
2024-05-06 16:54:55 +0530

    while loop and do while loop completed

---
+++ 05_Iteration/02_while_and_do_while.js
@@ -0,0 +1,22 @@
+let index = 1
+
+while (index <= 10) {
+    console.log(`value is ${index}`);
+    index++
+}
+
+console.log(`********************************************`);
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+
+console.log(`********************************************`);
+
+let number = 11
+do {
+    console.log(`number is ${number}`);
+    number++
+} while (number <= 10);
