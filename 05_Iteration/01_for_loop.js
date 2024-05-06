92f3cdadf3e44be8c1166b898f08af0f7e189e3d
07subhadip <subhadiphensh31102000@gmail.com>
2024-05-06 01:48:18 +0000

    array loop

--- 05_Iteration/01_for_loop.js
+++ 05_Iteration/01_for_loop.js
@@ -9,3 +9,11 @@ for (let i = 1; i <= 10; i++) {
         console.log(`${i} x ${j} = ${i*j}`);
     }  
 }
+
+let myArr = ["ironman" , "superman" , "spiderman" , "batman" , "wonder woman"]
+console.log(myArr.length);
+
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+}
