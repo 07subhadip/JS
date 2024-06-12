4285573bb8a21aae9b80b73e898dd2f3ce7ef0ce
07subhadip <subhadiphensh31102000@gmail.com>
2024-06-12 08:36:40 +0530

    mardown file for Oops

--- 09_advance_one/02_promises.js
+++ 09_advance_one/02_promises.js
@@ -34,7 +34,7 @@ promiseThree.then(function(user){
 
 const promiseFour = new Promise(function(resolve,reject){
     setTimeout(() => {
-        let error = false
+        let error = true
         if (!error) {
             resolve({username : "subhadip",password:123})
         }else{
@@ -54,3 +54,61 @@ promiseFour
 .catch(function(error){
     console.log(error); 
 })
+.finally(function(){
+    console.log("The promise is either resolve or rejected")
+})
+
+
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+        resolve({username:"Javascript",age:23})
+        }else{
+            reject("Error :JS went wrong")
+        }
+    }, 1000);
+})
+
+async function consumePromiseFive(){
+   try {
+    const response =  await promiseFive 
+    console.log(response);
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+consumePromiseFive() 
+
+
+
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E:",error);
+//     }
+// }
+
+
+//getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) =>{
+    if(!response.ok){
+        throw new Error("Network failed : "+response.statusText)
+    }else{
+        return response.json()
+    }
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error) => console.log(error));
---
+++ 10_Classes_Oops/notes.md
@@ -0,0 +1,25 @@
+# JavaScript and Classes
+
+## Oops
+
+### Object
+- Collection of Properties and methods
+- toLowerCase
+
+
+## Why use Oops?
+
+## parts of Oops
+Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new,this)
+
+
+## 4 pillars
+Abstraction <br>
+Encapsulation <br>
+Inheritance <br>
+Polymorphism <br>
